013ba63c78aab1c8ab58620b284122464204812f
Geevaveeri <59940979+Geevaveeri@users.noreply.github.com>
2021-07-09 22:24:15 -0600

    completed the add and delete friends routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -62,6 +62,41 @@ const userController = {
 			.then(dbUserData => res.json(dbUserData))
 			.catch(err => res.json(err));
 	},
+
+	// add a friend to users friend list
+	// /api/users/:id/friends/:friendsId
+	addFriend({ params }, res) {
+		User.findOneAndUpdate(
+			{ _id: params.id },
+			{ $addToSet: { friends: params.friendsId } },
+			{ new: true }
+		)
+			.then(dbUserData => {
+				if (!dbUserData) {
+					res.status(404).json({ message: "No User found with this id!" });
+					return;
+				}
+				res.json(dbUserData);
+			})
+			.catch(err => res.json(err));
+	},
+
+	// delete a friend from a users friend list
+	deleteFriend({ params }, res) {
+		User.findOneAndUpdate(
+			{ _id: params.id },
+			{ $pull: { friends: params.friendsId } },
+			{ new: true }
+		)
+			.then(dbUserData => {
+				if (!dbUserData) {
+					res.status(404).json({ message: "No User found with this id!" });
+					return;
+				}
+				res.json(dbUserData);
+			})
+			.catch(err => res.json(err));
+	},
 };
 
 module.exports = userController;
